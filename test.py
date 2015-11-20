7ddf088310778a67de69cc1158992d9646b0ec9f
patrick nsukami <ndkpatt@gmail.com>
2015-11-20 23:30:14 +0000

    use a non blocking socket + selector to wait for read/write event

--- test.py
+++ test.py
@@ -9,16 +9,40 @@
 
 import socket
 import time
+
+# 2° to wait for some event on a non blocking socket
+# jusk ask for the default selector for your system
+from selectors import DefaultSelector, EVENT_WRITE, EVENT_READ
+selector = DefaultSelector()
+
 # client socket to retrieve something from a server
 def get(path):
     s = socket.socket()
-    s.connect(('duckduckgo.com', 80))
+    s.setblocking(False) # socket no more blocking
+    try:
+        s.connect(('duckduckgo.com', 80)) # will return immediately or raise exception
+    except BlockingIOError:
+        pass
+
     request = 'GET %s HTTP/1.0\r\n\r\n' % path
+
+    # dear selector, I'm interested in any event that may occur on this file descriptor (my socket)
+    selector.register(s.fileno(), EVENT_WRITE)
+    # wait for what I'm intested in
+    selector.select()
+    # I'm no more interested in the write event on my socket, please forget about it
+    selector.unregister(s.fileno())
+    # socket is writable, so we can send
     s.send(request.encode()) # un-code
 
     chunks = []
     while True:
-
+        # dear selector, I'm interested in any event that may occur on this file descriptor (my socket)
+        selector.register(s.fileno(), EVENT_READ)
+        # wait for what I'm intested in
+        selector.select()
+        # I'm no more interested in the read event on my socket, please forget about it
+        selector.unregister(s.fileno())
         chunk = s.recv(1000)
         if chunk:
             chunks.append(chunk)
