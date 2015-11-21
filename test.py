4ec3b575e696dc6c23dc9431249cda40b1307840
patrick nsukami <ndkpatt@gmail.com>
2015-11-21 00:39:34 +0000

    callback ok, but we're stuck in an infinite loop

--- test.py
+++ test.py
@@ -26,37 +26,54 @@ def get(path):
 
     request = 'GET %s HTTP/1.0\r\n\r\n' % path
 
+    # we write a closure to pass s & request to our writable callback
+    callback = lambda: writable(s, request)
     # dear selector, I'm interested in any event that may occur on this file descriptor (my socket)
-    selector.register(s.fileno(), EVENT_WRITE)
-    # wait for what I'm intested in
-    selector.select()
+    selector.register(s.fileno(), EVENT_WRITE, data=callback)
+
+
+# 3° let's write a callback that will be called once the socket is writable
+def writable(s, request):
     # I'm no more interested in the write event on my socket, please forget about it
     selector.unregister(s.fileno())
     # socket is writable, so we can send
     s.send(request.encode()) # un-code
 
     chunks = []
-    while True:
-        # dear selector, I'm interested in any event that may occur on this file descriptor (my socket)
-        selector.register(s.fileno(), EVENT_READ)
-        # wait for what I'm intested in
-        selector.select()
-        # I'm no more interested in the read event on my socket, please forget about it
-        selector.unregister(s.fileno())
-        chunk = s.recv(1000)
-        if chunk:
-            chunks.append(chunk)
 
-        else: #empty chunk, server hang up
-            body = b''.join(chunks).decode() # be-code
-            print(body.split('\n')[0])
-            return
+    # we write a closure to pass s & chunks to our callback
+    callback = lambda: readable(s, chunks)
+    # dear selector, I'm interested in any read event that may occur on this file descriptor
+    selector.register(s.fileno(), EVENT_READ, data=callback)
+
+def readable(s, chunks):
+    # I'm no more interested in the read event on my socket, please forget about it
+    selector.unregister(s.fileno())
+    chunk = s.recv(1000)
+    if chunk:
+        chunks.append(chunk)
+        # we need to ckeck if socket still readable, until the last chunk
+        callback = lambda: readable(s, chunks)
+        # dear selector, I'm interested in any read event that may occur on this file descriptor
+        selector.register(s.fileno(), EVENT_READ, data=callback)
+    else: #empty chunk, server hang up
+        body = b''.join(chunks).decode() # be-code
+        print(body.split('\n')[0])
+
 
 start = time.time()
 get('/?q=python+socket+&t=lm&ia=about')
 get('/?q=golang+socket+&t=lm&ia=about')
 get('/?q=rust+socket+&t=lm&ia=about')
 get('/?q=erlang+socket+&t=lm&ia=about')
+
+while True:
+    # retrieve events
+    events = selector.select()
+    for event, mask in events:
+        cb = event.data
+        # whatever the event, call the callback linked to it
+        cb()
 
 # get launched serially, so final time is time for one request times number of requests
 print("took %.1f sec" % (time.time() - start))
